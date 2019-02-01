c2ee45a29573ab5f8276c07c70e6280af6c34b37
Michael M <Schnark@users.noreply.github.com>
2019-02-01 10:19:49 +0100

    Fix service worker

--- sw.js
+++ sw.js
@@ -2,7 +2,8 @@
 (function (worker) {
 "use strict";
 
-var VERSION = 'v1.3',
+var PREFIX = 'wallpaper',
+	VERSION = '1.3',
 	FILES = [
 		'index.html',
 		'style.css',
@@ -57,7 +58,7 @@ var VERSION = 'v1.3',
 
 worker.addEventListener('install', function (e) {
 	e.waitUntil(
-		caches.open(VERSION).then(function (cache) {
+		caches.open(PREFIX + ':' + VERSION).then(function (cache) {
 			return cache.addAll(FILES);
 		})
 	);
@@ -67,7 +68,7 @@ worker.addEventListener('activate', function (e) {
 	e.waitUntil(
 		caches.keys().then(function (keys) {
 			return Promise.all(keys.map(function (key) {
-				if (key !== VERSION) {
+				if (key.indexOf(PREFIX + ':') === 0 && key !== PREFIX + ':' + VERSION) {
 					return caches.delete(key);
 				}
 			}));
